928f575b5531591826db721e367df84980931e66
sing3demons <sing3demons@live.com>
2021-02-16 12:43:20 +0700

    first commit && setup reacte Material-UI

---
+++ src/modules/ui/components/Content.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div>
+      <h1>Content</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque numquam
+      alias quos, rerum suscipit dolorem dicta non fuga et nihil ipsum incidunt
+      quibusdam consequuntur sapiente pariatur facere cumque magni illo modi!
+      Veritatis rerum nisi quidem necessitatibus expedita sequi error? Eum,
+      placeat, magnam suscipit quod, id sunt pariatur ipsam assumenda eius a
+      iste esse. Odio eum quidem, culpa blanditiis autem aliquid quisquam
+      voluptate, commodi consequuntur itaque nobis enim impedit, maxime quas
+      accusamus quis debitis laboriosam temporibus deserunt dolorem iusto
+      dolorum totam. Omnis vitae, iure, quisquam tenetur voluptatum laboriosam
+      commodi ipsam fuga quo placeat nesciunt! In optio alias, ea impedit
+      eveniet consectetur iste corrupti similique fuga officiis porro excepturi
+      aliquid architecto inventore totam sequi obcaecati odit dignissimos
+      quaerat suscipit dolorem voluptate voluptatum unde. Quia voluptate aut
+      praesentium eius reprehenderit earum perferendis velit, nostrum tempore
+      accusamus ex autem qui saepe commodi sit ducimus omnis sapiente tempora
+      voluptatem non, amet blanditiis aspernatur at? Consequatur nulla animi
+      quos quia, saepe minus. Doloremque molestiae rerum id eos corrupti ex
+      dignissimos laudantium at quos. Eius, illo. Quidem, enim quisquam,
+      consectetur saepe hic esse eos unde voluptates dignissimos tenetur ratione
+      cum illum sed eum alias error officia. Unde velit dolorum enim sit, qui
+      quos quod quidem, numquam blanditiis corrupti minima ex quaerat a sed odit
+      atque, molestias quasi nostrum voluptate cum amet laudantium deleniti
+      eveniet voluptas? Minima officia laudantium facilis accusamus soluta omnis
+      magni, quisquam ex sit alias molestias illum adipisci fugit. Voluptates
+      veritatis consequatur recusandae molestias? Facere consectetur assumenda
+      quo eum magni, recusandae eveniet incidunt libero explicabo doloremque
+      atque ducimus neque. Excepturi voluptates at eveniet dolorem, fugit sunt
+      consequatur assumenda laudantium omnis a totam soluta debitis illum optio
+      unde. Molestias temporibus aut magnam illum sint rem enim expedita
+      asperiores, reiciendis architecto magni animi pariatur explicabo! Animi,
+      nam. Blanditiis laboriosam hic delectus mollitia magnam. Incidunt maxime
+      ex natus magnam, ratione pariatur consequuntur quidem nesciunt fuga. Quia
+      beatae veniam officiis, minima error quo, impedit, iure doloremque harum
+      molestias animi non tempora quibusdam illum ducimus! A ipsam veniam modi!
+      Sequi inventore tempore dicta sed nemo excepturi, beatae explicabo a
+      error. Inventore assumenda non rerum rem debitis beatae nostrum, illo
+      quidem eveniet cum sint incidunt tempore blanditiis, necessitatibus, nobis
+      dolore hic? Alias corporis nihil quisquam, quasi non architecto quod
+      consequatur corrupti molestiae dicta reprehenderit minus sunt maxime, quam
+      repudiandae, excepturi vitae sit consectetur hic facere odit animi atque
+      officia. Cum obcaecati nam, nesciunt sequi error facere aliquam doloremque
+      soluta earum numquam blanditiis quidem quam ex totam nisi dolores unde hic
+      iste natus. Accusamus in libero minima? Odio deserunt tempora assumenda
+      nihil consequuntur quae voluptatibus veniam laborum nobis, rem dolorem
+      cupiditate quisquam eveniet, ullam aspernatur facilis fugit, consequatur
+      dolorum delectus. A optio sapiente explicabo neque? Molestias quaerat unde
+      totam modi aperiam praesentium, inventore reiciendis sapiente
+      voluptatibus, repudiandae hic cupiditate veritatis consequuntur, alias
+      ratione quas vitae? Nesciunt excepturi quos quas minus eius iste
+      reiciendis reprehenderit asperiores maiores autem numquam optio similique,
+      dolorem sed sint error inventore aspernatur repellendus quo ut? Porro
+      veniam saepe impedit at deserunt? Similique obcaecati impedit aliquid,
+      quia dignissimos ratione?
+    </div>
+  )
+}
